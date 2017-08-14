6229e7d8b0107926a43c7d374bbd6a47b8cc6247
twoentartian <twoentartian@hotmail.com>
2017-08-14 18:07:37 +0800

    Add functions: refresh port, console

--- Cs_LAL_EegVisualization_1_1/FormMain.Designer.cs
+++ Cs_LAL_EegVisualization_1_1/FormMain.Designer.cs
@@ -33,6 +33,10 @@
 			this.menuStrip = new System.Windows.Forms.MenuStrip();
 			this.applicationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.buttonRefresh = new System.Windows.Forms.Button();
+			this.buttonOpenClose = new System.Windows.Forms.Button();
+			this.comboBoxSerialPort = new System.Windows.Forms.ComboBox();
+			this.textBoxConsole = new System.Windows.Forms.TextBox();
 			this.menuStrip.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -47,7 +51,7 @@
             this.applicationToolStripMenuItem});
 			this.menuStrip.Location = new System.Drawing.Point(0, 0);
 			this.menuStrip.Name = "menuStrip";
-			this.menuStrip.Size = new System.Drawing.Size(1315, 38);
+			this.menuStrip.Size = new System.Drawing.Size(912, 38);
 			this.menuStrip.TabIndex = 0;
 			this.menuStrip.Text = "menuStrip1";
 			// 
@@ -66,18 +70,65 @@
 			this.exitToolStripMenuItem.Text = "Exit";
 			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
 			// 
+			// buttonRefresh
+			// 
+			this.buttonRefresh.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonRefresh.Location = new System.Drawing.Point(139, 432);
+			this.buttonRefresh.Name = "buttonRefresh";
+			this.buttonRefresh.Size = new System.Drawing.Size(121, 38);
+			this.buttonRefresh.TabIndex = 1;
+			this.buttonRefresh.Text = "Refresh";
+			this.buttonRefresh.UseVisualStyleBackColor = true;
+			this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+			// 
+			// buttonOpenClose
+			// 
+			this.buttonOpenClose.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonOpenClose.Location = new System.Drawing.Point(266, 432);
+			this.buttonOpenClose.Name = "buttonOpenClose";
+			this.buttonOpenClose.Size = new System.Drawing.Size(121, 38);
+			this.buttonOpenClose.TabIndex = 2;
+			this.buttonOpenClose.Text = "Open";
+			this.buttonOpenClose.UseVisualStyleBackColor = true;
+			this.buttonOpenClose.Click += new System.EventHandler(this.buttonOpenClose_Click);
+			// 
+			// comboBoxSerialPort
+			// 
+			this.comboBoxSerialPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBoxSerialPort.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.comboBoxSerialPort.FormattingEnabled = true;
+			this.comboBoxSerialPort.Location = new System.Drawing.Point(12, 432);
+			this.comboBoxSerialPort.Name = "comboBoxSerialPort";
+			this.comboBoxSerialPort.Size = new System.Drawing.Size(121, 38);
+			this.comboBoxSerialPort.TabIndex = 3;
+			// 
+			// textBoxConsole
+			// 
+			this.textBoxConsole.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBoxConsole.Location = new System.Drawing.Point(12, 41);
+			this.textBoxConsole.Multiline = true;
+			this.textBoxConsole.Name = "textBoxConsole";
+			this.textBoxConsole.ReadOnly = true;
+			this.textBoxConsole.Size = new System.Drawing.Size(888, 380);
+			this.textBoxConsole.TabIndex = 5;
+			// 
 			// FormMain
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1315, 499);
+			this.ClientSize = new System.Drawing.Size(912, 526);
+			this.Controls.Add(this.textBoxConsole);
+			this.Controls.Add(this.comboBoxSerialPort);
+			this.Controls.Add(this.buttonOpenClose);
+			this.Controls.Add(this.buttonRefresh);
 			this.Controls.Add(this.menuStrip);
 			this.DoubleBuffered = true;
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
 			this.MainMenuStrip = this.menuStrip;
 			this.MaximizeBox = false;
 			this.Name = "FormMain";
 			this.Text = "EEG Visualizer";
+			this.Load += new System.EventHandler(this.FormMain_Load);
 			this.menuStrip.ResumeLayout(false);
 			this.menuStrip.PerformLayout();
 			this.ResumeLayout(false);
@@ -91,6 +142,10 @@
 		private System.Windows.Forms.MenuStrip menuStrip;
 		private System.Windows.Forms.ToolStripMenuItem applicationToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.Button buttonRefresh;
+		private System.Windows.Forms.Button buttonOpenClose;
+		private System.Windows.Forms.ComboBox comboBoxSerialPort;
+		private System.Windows.Forms.TextBox textBoxConsole;
 	}
 }
 
--- Cs_LAL_EegVisualization_1_1/FormMain.cs
+++ Cs_LAL_EegVisualization_1_1/FormMain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,40 @@ namespace Cs_LAL_EegVisualization_1_1
 			InitializeComponent();
 		}
 
+		#region Delegate
+
+		private delegate void WriteToConsoleHandler(string str);
+		public void WriteToConsoleError(string error)
+		{
+			if (textBoxConsole.InvokeRequired == true)
+			{
+				WriteToConsoleHandler set = new WriteToConsoleHandler(WriteToConsoleError);
+				textBoxConsole.Invoke(set, new object[] { error });
+			}
+			else
+			{
+				DateTime currentDateTime = DateTime.Now;
+				textBoxConsole.AppendText($"{currentDateTime.Hour:D2}:{currentDateTime.Minute:D2}:{currentDateTime.Second:D2}: Error - ");
+				textBoxConsole.AppendText(error + Environment.NewLine);
+			}
+		}
+		public void WriteToConsoleInfo(string info)
+		{
+			if (textBoxConsole.InvokeRequired == true)
+			{
+				WriteToConsoleHandler set = new WriteToConsoleHandler(WriteToConsoleInfo);
+				textBoxConsole.Invoke(set, new object[] { info });
+			}
+			else
+			{
+				DateTime currentDateTime = DateTime.Now;
+				textBoxConsole.AppendText($"{currentDateTime.Hour:D2}:{currentDateTime.Minute:D2}:{currentDateTime.Second:D2}: Info - ");
+				textBoxConsole.AppendText(info + Environment.NewLine);
+			}
+		}
+
+		#endregion
+
 
 		#region Button
 
@@ -25,7 +60,55 @@ namespace Cs_LAL_EegVisualization_1_1
 			Environment.Exit(0);
 		}
 
+		private void buttonRefresh_Click(object sender, EventArgs e)
+		{
+			SerialRefresh();
+		}
+
+		private void buttonOpenClose_Click(object sender, EventArgs e)
+		{
+
+		}
+
 		#endregion
 
+		#region Serial
+
+		private void SerialRefresh()
+		{
+			comboBoxSerialPort.Items.Clear();
+			string[] availablePorts = SerialPort.GetPortNames();
+			foreach (var port in availablePorts)
+			{
+				comboBoxSerialPort.Items.Add(port);
+			}
+			if (availablePorts.Length == 0)
+			{
+				WriteToConsoleError("No Port Found");
+			}
+			else if (availablePorts.Length == 1)
+			{
+				WriteToConsoleInfo($"Find 1 Port");
+				comboBoxSerialPort.SelectedIndex = 0;
+			}
+			else
+			{
+				WriteToConsoleInfo($"Find {availablePorts.Length:D} Ports");
+			}
+		}
+
+		#endregion
+
+		#region Form
+
+		private void FormMain_Load(object sender, EventArgs e)
+		{
+			WriteToConsoleInfo("Program Start");
+			SerialRefresh();
+		}
+
+		#endregion
+
+		
 	}
 }
