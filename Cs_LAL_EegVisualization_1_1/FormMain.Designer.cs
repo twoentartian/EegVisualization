e75f89e1e18f0a258f165d7b49a7818b100ba713
twoentartian <twoentartian@hotmail.com>
2017-08-14 17:05:25 +0800

    Add project files.

---
+++ Cs_LAL_EegVisualization_1_1/FormMain.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Cs_LAL_EegVisualization_1_1
+{
+	partial class FormMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.serialPort = new System.IO.Ports.SerialPort(this.components);
+			this.menuStrip = new System.Windows.Forms.MenuStrip();
+			this.applicationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// serialPort
+			// 
+			this.serialPort.BaudRate = 115200;
+			// 
+			// menuStrip
+			// 
+			this.menuStrip.ImageScalingSize = new System.Drawing.Size(28, 28);
+			this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.applicationToolStripMenuItem});
+			this.menuStrip.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip.Name = "menuStrip";
+			this.menuStrip.Size = new System.Drawing.Size(1315, 38);
+			this.menuStrip.TabIndex = 0;
+			this.menuStrip.Text = "menuStrip1";
+			// 
+			// applicationToolStripMenuItem
+			// 
+			this.applicationToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+			this.applicationToolStripMenuItem.Name = "applicationToolStripMenuItem";
+			this.applicationToolStripMenuItem.Size = new System.Drawing.Size(130, 34);
+			this.applicationToolStripMenuItem.Text = "Application";
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(239, 34);
+			this.exitToolStripMenuItem.Text = "Exit";
+			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+			// 
+			// FormMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1315, 499);
+			this.Controls.Add(this.menuStrip);
+			this.DoubleBuffered = true;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MainMenuStrip = this.menuStrip;
+			this.MaximizeBox = false;
+			this.Name = "FormMain";
+			this.Text = "EEG Visualizer";
+			this.menuStrip.ResumeLayout(false);
+			this.menuStrip.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.IO.Ports.SerialPort serialPort;
+		private System.Windows.Forms.MenuStrip menuStrip;
+		private System.Windows.Forms.ToolStripMenuItem applicationToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+	}
+}
+
---
+++ Cs_LAL_EegVisualization_1_1/FormMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cs_LAL_EegVisualization_1_1
+{
+	public partial class FormMain : Form
+	{
+		public FormMain()
+		{
+			InitializeComponent();
+		}
+
+
+		#region Button
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Environment.Exit(0);
+		}
+
+		#endregion
+
+	}
+}
