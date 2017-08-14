494f455966c678d21c8e9a4f24f02dffacdc8cb8
twoentartian <twoentartian@hotmail.com>
2017-08-14 21:25:35 +0800

    Add DataManager component and EEG panel.

---
+++ Cs_LAL_EegVisualization_1_1/DataManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs_LAL_EegVisualization_1_1
+{
+	class DataManager
+	{
+		#region Singleton
+
+		
+
+		#endregion
+	}
+}
---
+++ Cs_LAL_EegVisualization_1_1/FormEegVisualizer.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Cs_LAL_EegVisualization_1_1
+{
+	partial class FormEegVisualizer
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// FormEegVisualizer
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1515, 787);
+			this.Name = "FormEegVisualizer";
+			this.Text = "FormEegVisualizer";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
